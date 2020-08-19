63d150da6dc663a4c4ee85fd4073ee020c762464
Work & School <chouac@live.com>
2020-08-19 11:30:57 -0500

    Customer/Employee Models Created Dev created the Customer and Employee models along with adding a few variables to start (Most likely will have to revist as project progresses). At this time, Dev will continue with ID in ASP.NET Core to setup the project.

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName {get; set; }
+        public string LastName { get; set; }
+        public string OneTimePickup { get; set; }
+        public double Balance { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Address { get; set; }
+        public string Zipcode { get; set; }
+        public bool isConfirmed { get; set; }
+    }
+}
