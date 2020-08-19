c29775f45a98291bfe946cb09e126c7a2ceee0cb
Work & School <chouac@live.com>
2020-08-19 14:27:12 -0500

    ID Users and Roles Tables Completed.

--- TrashCollectorProject/Data/ApplicationDbContext.cs
+++ TrashCollectorProject/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TrashCollectorProject.Models;
@@ -14,6 +15,18 @@ namespace TrashCollectorProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+            .HasData(
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            }
+            );
         }
         public DbSet<TrashCollectorProject.Models.Customer> Customer { get; set; }
         public DbSet<TrashCollectorProject.Models.Employee> Employee { get; set; }
